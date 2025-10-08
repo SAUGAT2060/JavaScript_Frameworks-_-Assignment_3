b517b8a4f17e771ed7b95e2f51e4014ff707bb0f
Saugat Rana <ranasaugat9@gmail.com>
2025-10-07 21:29:15 -0400

    Done with the part1

--- index1.js
+++ index1.js
@@ -1,11 +1,29 @@
 import express from 'express'
-
+import data from './data/employees.json' with {type:'json'};
 const app =express()
 const port =3000
 
+
+
 app.get('/',(req,res)=>{
-  res.send('Hello World');
+res.send(`
+<html>
+      <head>
+        <title>Group Names</title>
+      </head>
+      <body>
+        <h1>Group 15 Members</h1>
+        <ul>
+          <li>Saugat Rana</li>
+          <li>Kushal Gurung</li>
+          <li>Lab_3-Assignment </li>
+        </ul>
+      </body>
+    </html>
+    
+  `)
 })
-app.listen(port,()=>{
-  console.log(`URL:http://localhost:${port}`)
+
+ app.listen(port,()=>{
+   console.log(`URL:http://localhost:${port}`)
 })
