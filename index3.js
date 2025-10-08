e06c7980eb6524dd5b6ba8b6e67f84b121fe3366
Saugat Rana <ranasaugat9@gmail.com>
2025-10-07 22:19:36 -0400

    Done with Part3

--- index3.js
+++ index3.js
@@ -4,9 +4,17 @@ const app = express()
 const port = 3000
 
 app.get('/get',(req,res)=>{
-  res.send ("Learned about get request")
+  res.send ("Learned about get method")
 })
-
-app.listen(post,()=>{
+app.post('/post',(req,res)=>{
+  res.send("Learned about post method")
+})
+app.put('/put',(req,res)=>{
+  res.send("Learned about put method")
+})
+app.delete('/delete',(req,res)=>{
+  res.send("Learned about delete method")
+})
+app.listen(port,()=>{
   console.log(`URL:http://localhost:${port}`)
 })
